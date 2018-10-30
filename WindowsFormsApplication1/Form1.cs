cc2ddccfbc13bb1e473e00132d5f1dc4c8f75a5f
denisVB <denis@fateychik.ru>
2018-10-30 22:13:01 +0300

    Added map loading and robot map drawing

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -17,13 +17,16 @@ namespace WindowsFormsApplication1
 		public Form1()
 		{
 			InitializeComponent();
-			GetMode();
-		}
+            InitializeComponents();
+            InterfaceDraw();
+        }
 
 		int x, y; //значения размерности
 		bool draw = false;
         bool rightMouseButton = false;
 		int[,] mapArray;
+        int[,] robotMapArray;
+        bool currentMap = false; //0 - global, 1 - robot
         Graph mapGraph = new Graph();
 
 		//отрисовка элементов карты
@@ -33,91 +36,159 @@ namespace WindowsFormsApplication1
 		PictureBox globalMapPictureBox = new PictureBox();
 		Bitmap globalMap;
 		Graphics globalMapGraphics;
+        Bitmap robotMap;
+        Graphics robotMapGraphics;
 
 		Button saveButton = new Button ();
 		Button loadButton = new Button ();
 		Button createButton = new Button ();
+        Button switchMapButton = new Button();
 		Label xTrackBarLabel = new Label ();
 		Label yTrackBarLabel = new Label ();
 		TrackBar xTrackBar = new TrackBar ();
 		TrackBar yTrackBar = new TrackBar ();
-		Size buttonSize = new Size (100, 25);
+
+        CheckedListBox savedMaps = new CheckedListBox();
+        Button savedMapsButton = new Button();
+
+        Size buttonSize = new Size (100, 25);
 
 		Pen emptyRectPen = new Pen(Color.Gray, lineWidth); //линия пустой клетки
 		SolidBrush takenRectBrush = new SolidBrush(Color.Black); //зарисовка занятой клетки
         SolidBrush emptyRectBrush = new SolidBrush(Color.LightGray);
-		//
+        SolidBrush unknownRectBrush = new SolidBrush(Color.LightBlue);
+        //
+
+        void InitializeComponents()
+        {
+            this.Size = new Size(500, 350); //окно программы
+
+            loadButton.Location = new Point(10, 10);
+            loadButton.Size = buttonSize;
+            loadButton.Text = "Загрузить";
+            loadButton.MouseClick += new MouseEventHandler(LoadButtonClick);
+
+            saveButton.Location = new Point(125, 10);
+            saveButton.Size = buttonSize;
+            saveButton.Text = "Сохранить";
+            saveButton.MouseClick += new MouseEventHandler(SaveButtonClick);
+
+            createButton.Location = new Point(240, 10);
+            createButton.Size = buttonSize;
+            createButton.Text = "Создать";
+            createButton.MouseClick += new MouseEventHandler(CreateButtonClick);
+
+            switchMapButton.Location = new Point(355, 10);
+            switchMapButton.Size = buttonSize;
+            switchMapButton.Text = "Карта";
+            switchMapButton.MouseClick += new MouseEventHandler(SwitchMap);
+
+            xTrackBarLabel.Location = new Point(10, 50);
+            xTrackBarLabel.Size = new Size(40, 20);
+
+            xTrackBar.Location = new Point(50, 50);
+            xTrackBar.Minimum = 10;
+            xTrackBar.Maximum = 100;
+            xTrackBar.TickFrequency = 10;
+            xTrackBar.Scroll += TrackBarScroll;
+
+            yTrackBarLabel.Location = new Point(170, 50);
+            yTrackBarLabel.Size = new Size(40, 20);
+
+            yTrackBar.Location = new Point(210, 50);
+            yTrackBar.Minimum = 10;
+            yTrackBar.Maximum = 100;
+            yTrackBar.TickFrequency = 10;
+            yTrackBar.Scroll += TrackBarScroll;
+
+            savedMaps.CheckOnClick = true;
+            savedMaps.SelectionMode = SelectionMode.One;
+            savedMaps.Location = new Point(10, 100);
+            savedMaps.Size = new Size(200, 200);
+
+            savedMapsButton.Location = new Point(230, 100);
+            savedMapsButton.Size = buttonSize;
+            savedMapsButton.Text = "Загрузить";
+            savedMapsButton.MouseClick += new MouseEventHandler(SavedMapChoiceButtonClick);
+
+            globalMapPictureBox.MouseMove += new MouseEventHandler(GlobalMapMouseMove);
+            globalMapPictureBox.MouseUp += new MouseEventHandler(GlobalMapMouseUp);
+            globalMapPictureBox.MouseDown += new MouseEventHandler(GlobalMapMouseDown);
+
+            emptyRectPen.Alignment = PenAlignment.Inset; //закрашивание внутри контура
+
+            globalMapPictureBox.Location = new Point(10, 100); //размещение карты в окне
+
+            Controls.Add(globalMapPictureBox);
 
 
-		void GetMode()
-		{
-			this.Size = new Size(500, 350); //окно программы
+        }
 
-			loadButton.Location = new Point (10, 10);
-			loadButton.Size = buttonSize;
-			loadButton.Text = "Загрузить";
-			loadButton.MouseClick += new MouseEventHandler (LoadButtonClick);
-			Controls.Add (loadButton);
+        void InterfaceDraw()
+        {
+            Controls.Add(loadButton);
+            Controls.Add(saveButton);
+            Controls.Add(createButton);
+            Controls.Add(xTrackBarLabel);
+            Controls.Add(xTrackBar);
+            Controls.Add(yTrackBarLabel);
+            Controls.Add(yTrackBar);
+            Controls.Add(switchMapButton);
+        }
 
-			saveButton.Location = new Point (125, 10);
-			saveButton.Size = buttonSize;
-			saveButton.Text = "Сохранить";
-			saveButton.MouseClick += new MouseEventHandler (SaveButtonClick);
-			Controls.Add (saveButton);
+        void SwitchMap(object sender, EventArgs e)
+        {
+            globalMapPictureBox.Image = currentMap ? globalMap : robotMap;
+            currentMap = !currentMap;
+        }
 
-			createButton.Location = new Point (240, 10);
-			createButton.Size = buttonSize;
-			createButton.Text = "Создать";
-			createButton.MouseClick += new MouseEventHandler (CreateButtonClick);
-			Controls.Add (createButton);
-
-			xTrackBarLabel.Location = new Point (10, 50);
-			xTrackBarLabel.Size = new Size(40,20);
-			Controls.Add (xTrackBarLabel);
-
-			xTrackBar.Location = new Point (50, 50);
-			xTrackBar.Minimum = 10;
-			xTrackBar.Maximum = 100;
-			xTrackBar.TickFrequency = 10;
-			xTrackBar.Scroll += TrackBarScroll;
-			Controls.Add (xTrackBar);
-
-			yTrackBarLabel.Location = new Point (170, 50);
-			yTrackBarLabel.Size = new Size(40,20);
-			Controls.Add (yTrackBarLabel);
-
-			yTrackBar.Location = new Point (210, 50);
-			yTrackBar.Minimum = 10;
-			yTrackBar.Maximum = 100;
-			yTrackBar.TickFrequency = 10;
-			yTrackBar.Scroll += TrackBarScroll;
-			Controls.Add (yTrackBar);
-		}
-
-		void TrackBarScroll (object sender, EventArgs e)
+        void TrackBarScroll(object sender, EventArgs e)
 		{
 			x = xTrackBar.Value;
 			y = yTrackBar.Value;
 			xTrackBarLabel.Text = String.Format ("X: {0}", xTrackBar.Value);
 			yTrackBarLabel.Text = String.Format ("Y: {0}", yTrackBar.Value);
-		}
+		} //сдвиг ползунка
 
 		void LoadButtonClick(object sender, EventArgs e)
 		{
-            var file = File.ReadAllLines(@"E:\map" + ".txt"); // хз как путь выбирать(мб создать меню с имеющимися файлами)
+            Controls.Remove(globalMapPictureBox);
+
+            string[] filesNames = Directory.GetFiles(@"c:\MARS maps");
+
+            savedMaps.Items.AddRange(filesNames);
+            Controls.Add(savedMaps);
+
+            Controls.Add(savedMapsButton);
+
+        } //нажание на кнопку загрузки
+
+        void SavedMapChoiceButtonClick(object sender, EventArgs e) //нажатие на кнопку выбора сохранённой карты
+        {
+            var file = File.ReadAllLines((string)savedMaps.SelectedItem); // хз как путь выбирать(мб создать меню с имеющимися файлами)
             mapArray = new int[file.Length, file[0].Length / 2];
+            robotMapArray = new int[file.Length, file[0].Length / 2];
+
+            y = file.Length;
+            x = file[0].Length / 2;
+
+            CreateMap();
 
             for (int i = 0; i < file.Length; i++)
             {
                 var temp = file[i].Split(' ');
-                for (int j = 0; j < temp.Length; j++)
+                for (int j = 0; j < temp.Length - 1; j++) //последний элемент строки - символ окончания строки
                     mapArray[i, j] = int.Parse(temp[j]);
             }
 
             DrawMap();
-        } //нажание на кнопку загрузки
+            savedMaps.Items.Clear();
+            Controls.Remove(savedMaps);
+            Controls.Remove(savedMapsButton);
+            Controls.Add(globalMapPictureBox);
+        }
 
-		void SaveButtonClick(object sender, EventArgs e)
+        void SaveButtonClick(object sender, EventArgs e)
 		{
             string fileName = System.IO.Path.Combine(@"c:\MARS maps", System.IO.Path.GetRandomFileName());
 
@@ -135,41 +206,67 @@ namespace WindowsFormsApplication1
 		void CreateButtonClick(object sender, EventArgs e)
 		{
 			mapArray = new int[y, x];
-			DrawMap ();
+            robotMapArray = new int[y, x];
+            CreateMap ();
 		} //нажатие на кнопку создания карты
 
 		// начало отрисовки интерфейса и карты
-		void DrawMap()
+		void CreateMap()
 		{
-			globalMapPictureBox.MouseMove += new MouseEventHandler(GlobalMapMouseMove);
-			globalMapPictureBox.MouseUp += new MouseEventHandler(GlobalMapMouseUp);
-			globalMapPictureBox.MouseDown += new MouseEventHandler(GlobalMapMouseDown);
 
-
-			emptyRectPen.Alignment = PenAlignment.Inset; //закрашивание внутри контура
 
 			this.Size = new Size(x * sideSize + 300, y * sideSize + 150); //окно программы
 
-			globalMapPictureBox.Location = new Point (10, 100); //размещение карты в окне
+
 			globalMapPictureBox.Size = new Size(x * sideSize+1, y * sideSize+1); //окно карты
-			Controls.Add(globalMapPictureBox);
+
+
 			globalMap = new Bitmap(x * sideSize+1, y * sideSize+1);
-			globalMapGraphics = Graphics.FromImage(globalMap);
 
-			for (int i = 0; i < y; i++) //отрисовка пустой карты
-			{
-				for (int j = 0; j < x; j++)
-				{
-                    globalMapGraphics.FillRectangle(takenRectBrush, j * (sideSize), i * (sideSize), sideSize + 1, sideSize + 1);
-                    globalMapGraphics.DrawRectangle(emptyRectPen, j * (sideSize), i * (sideSize), sideSize, sideSize);
+            robotMap = new Bitmap(x * sideSize + 1, y * sideSize + 1);
+
+            globalMapGraphics = Graphics.FromImage(globalMap);
+
+            robotMapGraphics = Graphics.FromImage(robotMap);
+
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
                     mapArray[i, j] = 1;
-				}
-			}
+                    robotMapArray[i, j] = 2;
+                }
+            }
 
-			globalMapPictureBox.Image = globalMap;
+            DrawMap();
 
 		} //отрисовка карты
-		//конец отрисовки интерфейса и карты
+          //конец отрисовки интерфейса и карты
+
+        void DrawMap()
+        {
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    if (mapArray[i, j] == 1)
+                        globalMapGraphics.FillRectangle(takenRectBrush, j * (sideSize), i * (sideSize), sideSize + 1, sideSize + 1);
+                    else
+                        globalMapGraphics.FillRectangle(emptyRectBrush, j * sideSize, i * sideSize, sideSize + 1, sideSize + 1);
+                    globalMapGraphics.DrawRectangle(emptyRectPen, j * (sideSize), i * (sideSize), sideSize, sideSize);
+
+                    if (robotMapArray[i, j] == 1)
+                        robotMapGraphics.FillRectangle(takenRectBrush, j * (sideSize), i * (sideSize), sideSize + 1, sideSize + 1);
+                    if (robotMapArray[i, j] == 0)
+                        robotMapGraphics.FillRectangle(emptyRectBrush, j * sideSize, i * sideSize, sideSize + 1, sideSize + 1);
+                    if (robotMapArray[i, j] == 2)
+                        robotMapGraphics.FillRectangle(unknownRectBrush, j * sideSize, i * sideSize, sideSize + 1, sideSize + 1);
+                    robotMapGraphics.DrawRectangle(emptyRectPen, j * (sideSize), i * (sideSize), sideSize, sideSize);
+                }
+            }
+
+            globalMapPictureBox.Image = globalMap;
+        }
 
 		private void GlobalMapMouseMove(object sender, MouseEventArgs e)
 		{
